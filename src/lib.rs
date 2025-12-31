ad9d4d5b46b37a8a67e616b0901f553272373344
Ovikx <ovikdas10@gmail.com>
2025-12-31 13:33:26 -0500

    coerce type instead of explicit casting

--- src/lib.rs
+++ src/lib.rs
@@ -44,14 +44,15 @@ impl WasmSampleGenerator {
     pub fn load_graph(&mut self, graph: JsValue) {
         let graph: SourceGraph = serde_wasm_bindgen::from_value(graph).unwrap();
         let nodes: Vec<Rc<RefCell<dyn Source>>> =  graph.nodes.iter().map(|node: &JSNode| {
-            match node {
-                JSNode::MultiplyNodeJS(multiply_node_js) => rc_refcell!(MultiplyNode::new(multiply_node_js.metadata.id, multiply_node_js.multiplicand_source_id, multiply_node_js.multiplier_source_id)) as Rc<RefCell<dyn Source>>,
-                JSNode::SumNodeJS(sum_node_js) => rc_refcell!(SumNode::new(sum_node_js.metadata.id, sum_node_js.augend_source_id, sum_node_js.addend_source_id)) as Rc<RefCell<dyn Source>>,
+            let core_node: Rc<RefCell<dyn Source>> = match node {
+                JSNode::MultiplyNodeJS(multiply_node_js) => rc_refcell!(MultiplyNode::new(multiply_node_js.metadata.id, multiply_node_js.multiplicand_source_id, multiply_node_js.multiplier_source_id)),
+                JSNode::SumNodeJS(sum_node_js) => rc_refcell!(SumNode::new(sum_node_js.metadata.id, sum_node_js.augend_source_id, sum_node_js.addend_source_id)) ,
                 JSNode::SineOscillatorNodeJS(sine_oscillator_node_js) => rc_refcell!(SineOscillatorNode::new(sine_oscillator_node_js.metadata.id, sine_oscillator_node_js.sine_frequency_source_id)),
                 JSNode::SawOscillatorNodeJS(saw_oscillator_node_js) => rc_refcell!(SawOscillatorNode::new(saw_oscillator_node_js.metadata.id, saw_oscillator_node_js.saw_frequency_source_id)),
                 JSNode::FloatSourceJS(float_source_js) => rc_refcell!(FloatSource::new(float_source_js.metadata.id, float_source_js.value)),
                 JSNode::SplineFloatNode(spline_float_node) => rc_refcell!(SplineFloatNode::new(spline_float_node.metadata.id, spline_float_node.frequency_source_id, spline_float_node.points.iter().map(|point| (point.x, point.y)).collect())),
-            }
+            };
+            core_node
         }).collect();
 
         self.generator = SampleGenerator::new(
